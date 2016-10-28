0302231c536f703d651a9de014731df04997b7ec
jq91 <jasieq91@gmail.com>
2016-10-28 17:41:54 +0200

    Initial commit

---
+++ config.rb
@@ -0,0 +1,10 @@
+preferred_syntax = :scss # or :sass
+http_path = '/'
+css_dir = 'css'
+sass_dir = 'scss'
+images_dir = 'images'
+javascripts_dir = 'js'
+relative_assets = true
+line_comments = false # or true
+output_style = :expanded # :nested, :expanded, :compact, or :compressed.
+environment = :development # :development can also be :production
